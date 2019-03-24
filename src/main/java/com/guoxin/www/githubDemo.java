19f76d8adf6ee1509f517134a5d3d2074d644c08
gx251zyb <1123221743@qq.com>
2019-03-24 19:13:19 +0800

    111111111111111111222222222222222222222

--- src/main/java/com/guoxin/www/githubDemo.java
+++ src/main/java/com/guoxin/www/githubDemo.java
@@ -8,8 +8,5 @@ public class githubDemo {
 		System.out.println("eclipse---------------2��ͻ------------------");
 		System.out.println("idea----------------------------------ct");
 		System.out.println("eclipse---------------2��ͻ------------------");
-		System.out.println("000000000000000000000000000000000000000000000");
-		System.out.println("2222222222222222222222222222222222222222222222222222222");
 	}
-
 }
