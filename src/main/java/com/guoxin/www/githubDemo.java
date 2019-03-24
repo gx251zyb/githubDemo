2a2df80e7ed7e38cccb3e5db88bccd34501d4b5c
gx251zyb <1123221743@qq.com>
2019-03-24 18:35:38 +0800

    eclipse 提交冲突1

--- src/main/java/com/guoxin/www/githubDemo.java
+++ src/main/java/com/guoxin/www/githubDemo.java
@@ -3,6 +3,7 @@ package com.guoxin.www;
 public class githubDemo {
 	public static void main(String[] args) {
 		System.out.println("------------eclipse--------------");
+		System.out.println("eclipse---------------1------------------");
 	}
 
 }
