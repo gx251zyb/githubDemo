8055ebeb87d0308be10301bf0b0cb7de22d49c2b
gx251zyb <1123221743@qq.com>
2019-03-24 18:59:13 +0800

    提交修改到本地仓库中

--- src/main/java/com/guoxin/www/githubDemo.java
+++ src/main/java/com/guoxin/www/githubDemo.java
@@ -5,6 +5,7 @@ public class githubDemo {
 		System.out.println("------------eclipse--------------");
 		System.out.println("eclipse---------------1------------------");
 		System.out.println("111111111111111111111111111111111111111111");
+		System.out.println("idea----------------------------------ct");
 	}
 
 }
